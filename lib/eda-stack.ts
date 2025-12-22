ff0a607829f242816f6e4e15b2a0e0aad7d7751e
mzkmnk <mzk.mnk.dev@gmail.com>
2025-12-22 12:57:51 +0900

    feat(cdk): add EventBus to EDA stack infrastructure
    - Import AWS Events module from aws-cdk-lib
    - Create public EventBus property in EdaStack class
    - Initialize EventBus with name "order-events" for order event routing
    - Add CloudFormation output to expose EventBus ARN for cross-stack references
    - Enable event-driven architecture foundation for order processing workflows

--- lib/eda-stack.ts
+++ lib/eda-stack.ts
@@ -1,8 +1,19 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as events from "aws-cdk-lib/aws-events";
 
 export class EdaStack extends cdk.Stack {
+  public readonly eventBus: events.EventBus;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    this.eventBus = new events.EventBus(this, "orderEventsBus", {
+      eventBusName: "order-events"
+    })
+
+    new cdk.CfnOutput(this, "EventBusArn",  {
+      value: this.eventBus.eventBusArn,
+      description: "Order Event Bus Arn"
+    })
   }
 }
